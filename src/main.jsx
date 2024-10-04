a11c36c240fdbbb2f4a8f509b157d4799a567f0c
Re_Go <jongmin0919@naver.com>
2024-10-04 17:22:47 +0900

    수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createHashRouter,
+} from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.jsx';
